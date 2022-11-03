27953d75baadc63ce38fe9a5e2f906555789fe35
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-11-03 14:26:00 -0300

    changings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { JwPaginationModule } from 'jw-angular-pagination';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AtualizarComponent } from './views/atualizar/atualizar.component';
-import { EmptyComponent } from './empty/empty.component';
+import { EmptyComponent } from './views/empty/empty.component';
 
 @NgModule({
   declarations: [
